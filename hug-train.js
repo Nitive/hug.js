b2dec5431616fa59a3f15e8cdf5d4c5e7e534284
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-07-31 17:03:26 +0700

    feat: add debounce

--- hug-train.js
+++ hug-train.js
@@ -2,3 +2,14 @@ var $ = function (selector) {
   return document.querySelector(selector)
 }
 
+var debounce = function (delay, cb) {
+  var prev = 0
+  return function () {
+    var now = Date.now()
+    if (now > prev + delay) {
+      prev = now
+      cb.apply(null, arguments)
+    }
+  }
+}
+
