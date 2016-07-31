17365b4f48f28e0fb8031f8a3e82adc8fc9143a7
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-07-31 17:04:32 +0700

    feat: add creating and deleting hugs

--- hug-train.js
+++ hug-train.js
@@ -13,6 +13,12 @@ var debounce = function (delay, cb) {
   }
 }
 
+var createHug = function () {
+  var hug = document.createElement('img')
+  hug.src = 'hug.gif'
+  return hug
+}
+
 var setCSS = function (styles, element) {
   Object.keys(styles).forEach(function (rule) {
     element.style[rule] = styles[rule] // eslint-disable-line no-param-reassign
@@ -33,3 +39,43 @@ var animate = function (time, timingFunction) {
   animateHelper()
 }
 
+var disappearElement = function (time, element) {
+  animate(time, function (passed) {
+    setCSS({ opacity: 1 - passed }, element)
+  })
+  setTimeout(function () {
+    element.remove()
+  }, time)
+}
+
+
+var HUG_SIDE = 44
+
+
+var main = function () {
+  var hugs = document.createElement('div')
+
+  var addHug = function (coords) {
+    var hug = createHug()
+    setCSS({
+      position: 'fixed',
+      top: coords.y + 'px',
+      left: coords.x + 'px'
+    }, hug)
+    hugs.appendChild(hug)
+    disappearElement(1500, hug)
+  }
+
+  var move = debounce(1, function (event) {
+    addHug({
+      x: event.clientX - (HUG_SIDE / 2),
+      y: event.clientY - (HUG_SIDE / 2)
+    })
+  })
+
+  $('body').appendChild(hugs)
+  $('html').addEventListener('mousemove', move)
+}
+
+
+main()
